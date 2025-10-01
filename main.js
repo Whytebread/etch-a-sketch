1bc357c1d502c5299e47bc3bec076695de605538
Whytebread <196115419+Whytebread@users.noreply.github.com>
2025-10-01 14:23:48 -0500

    set div size to create grid within the container

--- main.js
+++ main.js
@@ -6,5 +6,7 @@ for (i = 0; i <= 256; i++) {
     let div = document.createElement("div");
     div.className = "square";
     div.style.border= "solid black";
+    div.style.width= "50px";
+    div.style.height= "50px";
     container.appendChild(div);
 }

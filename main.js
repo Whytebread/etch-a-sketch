723125d9a406e0113c1cdb1a918c309a73e29197
Whytebread <196115419+Whytebread@users.noreply.github.com>
2025-10-04 13:40:06 -0500

    created inital version of createGrid function

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
 // Global variables
 const container = document.querySelector(".grid-container");
+const gridButton = document.querySelector(".grid-button");
 
 // loop for creating divs and appending to container div
 for (i = 0; i < 256; i++) {
     let div = document.createElement("div");
     div.className = "square";
-    div.style.border= "solid black 1px";
-    div.style.width= "60px";
-    div.style.height= "60px";
+    div.style.border = "solid black 1px";
+    div.style.width = "60px";
+    div.style.height = "60px";
     container.appendChild(div);
 
     // event listenter to change inital grid color
 
-div.addEventListener("mouseover", ()=> {
-    div.style.backgroundColor = "black";
-})
+    div.addEventListener("mouseover", () => {
+        div.style.backgroundColor = "black";
+    })
 
 }
 
-// function createGrid {
-//     container.innerHTML=""
-    
-// }
+function createGrid() {
+    let gridNumber = parseInt(prompt("Enter the number of squares per side (max 100)"));
+    container.innerHTML = ""
+    if (gridNumber > 100) {
+        let gridNumber = parseInt(prompt("Please enter a number less than 100"));
+    } else {
+        for (i = 0; i < gridNumber; i++) {
+            let div = document.createElement("div");
+            div.className = "square";
+            div.style.border = "solid black 1px";
+            div.style.width = 960 / gridNumber;
+            div.style.height = auto;
+            container.appendChild(div);
+
+            div.addEventListener("mouseover", () => {
+                div.style.backgroundColor = "black";
+            })
+        }
+    }
+
+}
+
+gridButton.addEventListener("click", createGrid());
